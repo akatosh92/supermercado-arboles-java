303345ea0a1b89e83a140c39b033d11fccc24091
cristian <cristian@localhost>
2026-04-22 22:36:32 -0600

    Add search, traversal and total calculation to BST

--- src/estructura/ArbolBinarioBusqueda.java
+++ src/estructura/ArbolBinarioBusqueda.java
@@ -36,4 +36,64 @@ private Nodo insertarRecursivo(Nodo actual, Producto producto) {
 
     return actual;
 }
+
+public Producto buscar(int id) {
+    Nodo resultado = buscarRecursivo(raiz, id);
+
+    if (resultado != null) {
+        return resultado.producto;
+    }
+
+    return null;
+}
+
+private Nodo buscarRecursivo(Nodo actual, int id) {
+
+    if (actual == null) {
+        return null;
+    }
+
+    if (actual.producto.getId() == id) {
+        return actual;
+    }
+
+    if (id < actual.producto.getId()) {
+        return buscarRecursivo(actual.izquierdo, id);
+    } else {
+        return buscarRecursivo(actual.derecho, id);
+    }
+}
+
+public void mostrarInOrden() {
+    inOrdenRecursivo(raiz);
+}
+
+private void inOrdenRecursivo(Nodo actual) {
+
+    if (actual != null) {
+
+        inOrdenRecursivo(actual.izquierdo);
+
+        System.out.println(actual.producto);
+
+        inOrdenRecursivo(actual.derecho);
+    }
+}
+
+public double calcularTotal() {
+    return calcularTotalRecursivo(raiz);
+}
+
+private double calcularTotalRecursivo(Nodo actual) {
+
+    if (actual == null) {
+        return 0;
+    }
+
+    double subtotal = actual.producto.calcularSubtotal();
+
+    return subtotal +
+           calcularTotalRecursivo(actual.izquierdo) +
+           calcularTotalRecursivo(actual.derecho);
+}
 }
